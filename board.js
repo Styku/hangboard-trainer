491480818754a5c856867edb4d0dcc097176acf4
Bartlomiej Styczen <brtstyczen@gmail.com>
2021-11-29 09:13:59 +0100

    timer

--- board.js
+++ board.js
@@ -1,3 +1,45 @@
+class Timer {
+    start_timestamp;
+    display_element;
+    handle;
+
+    constructor(display_element) {
+        this.display_element = document.getElementById(display_element);
+        this.display();
+    }
+
+    display() {
+        let time_passed = 0;
+        if(this.start_timestamp) {
+            time_passed = this.start_timestamp.getTime() - (new Date()).getTime();
+        }
+        if(time_passed < 0 ) {
+            time_passed = 0;
+        }
+        const ms = String(time_passed % 1000).padStart(3, '0');
+        time_passed = Math.floor(time_passed/1000);
+        const s = String(time_passed % 60).padStart(2, '0');
+        time_passed = Math.floor(time_passed/60);
+        const m = String(time_passed % 60).padStart(2, '0');
+        time_passed = Math.floor(time_passed/24);
+        const h = String(time_passed % 24).padStart(2, '0');
+        const time_string = `${h}:${m}:${s}:${ms}`;
+
+        this.display_element.innerText = time_string;
+
+        if(time_passed < 0 && this.handle) {
+            time_passed = 0;
+            clearInterval(this.handle);
+        }
+    }
+
+    start(time) {
+        this.start_timestamp = new Date();
+        this.start_timestamp.setSeconds(this.start_timestamp.getSeconds() + time);
+        this.handle = setInterval(this.display.bind(this), 5);
+    }
+}
+
 class Hang {
     width;
     height;
@@ -10,18 +52,15 @@ class Hang {
         this.width = w;
         this.height = h;
     }
-
-    scaled_size(scale_x, scale_y, px, py) {
-        return [this.x * scale_x + px, this.y * scale_y + py, this.width * scale_x - 2*px, this.height * scale_y - 2*py];
-    }
-
-    scaled_center(scale_x, scale_y) {
-        return [(this.x + this.width/2) * scale_x, (this.y + this.height/2) * scale_y]
-    }
 }
 
 class Hangboard {
-    hangs = []
+    hangs = [];
+    active_hangs = new Set();
+
+    get active() { return this.active_hangs; }
+    set active(active) { this.active_hangs = new Set(active); }
+    get size() { return this.hangs.length; }
 
     update(str) {
         this.hangs = [];
@@ -42,21 +81,15 @@ class Hangboard {
             y += height;
         }
     }
-
-    get size() {
-        return this.hangs.length;
-    }
 }
 
 class Canvas {
     container;
     canvas;
     context;
-    board_image;
-    state;
     hangboard = new Hangboard();
-    padding = 0.02;
-    border = 0.1;
+    padding_ratio = 0.02;
+    border_ratio = 0.07;
     editor;
     texture;
 
@@ -75,6 +108,7 @@ class Canvas {
             this.draw();
         }.bind(this), false);
         this.setHangs();
+        this.hangboard.active = [2, 5, 8, 12];
         this.resize();
     }
 
@@ -95,65 +129,89 @@ class Canvas {
         if(this.board_image) this.context.drawImage(this.board_image, 0, 0, this.width, this.height);
         this.drawHangs();
     }
+    
+    get margin() {
+        return { x: this.border_ratio * this.width, y: this.border_ratio * this.height };
+    }
+
+    get padding() {
+        return { x: this.padding_ratio * this.width, y: this.padding_ratio * this.height };
+    }
+
+    scale(hang) {
+        const scale_x = this.width - 2 * this.margin.x;
+        const scale_y = this.height - 2 * this.margin.y;
+        return [
+            hang.x * scale_x + this.padding.x + this.margin.x, 
+            hang.y * scale_y + this.padding.y + this.margin.y, 
+            hang.width * scale_x - 2 * this.padding.x, 
+            hang.height * scale_y - 2 * this.padding.y
+        ];
+    }
 
     drawHangs() {
         console.log("drawHangs called");
-        const px = this.padding * this.width;
-        const py = this.padding * this.height;
-        const mx = this.border * this.width;
-        const my = this.border * this.height;
-        for(const [i, hang] of this.hangboard.hangs.entries()) {
-            let rect = hang.scaled_size(this.width - 2*mx, this.height - 2*my, px, py);
-            rect[0] += mx;
-            rect[1] += my;
-            roundedRect(this.context, ...rect, px*2);
-            const center = hang.scaled_center(this.width-2*mx, this.height-2*my);
-            center[0] += mx;
-            center[1] += my;
 
-            let font_height = rect[3]*0.6;
-            this.context.font = `${font_height}px Courier New`;
-            let text_width = this.context.measureText(i).width;
-            if( text_width > rect[2]* 0.8) {
-                font_height *= rect[2]* 0.8 / text_width;
-                this.context.font = `${font_height}px Courier New`;
-                text_width = this.context.measureText(i).width;
+        for(const [i, hang] of this.hangboard.hangs.entries()) {
+            const rect = this.scale(hang);
+            console.log(this.hangboard.active);
+            if(this.hangboard.active.has(i)) {
+                this.context.fillStyle = "rgba(0, 0, 255, 0.2)";
+            } else {
+                this.context.fillStyle = "rgba(0, 0, 0, 0.2)";
             }
-            this.context.fillText(i, center[0] - text_width/2, center[1] + font_height/4);
+            this.roundedRectFilled(...rect, this.padding.x*2);
+            this.writeText(i, ...rect);
         }
     }
 
+    writeText(text, x, y, w, h) {
+        let font_height = h * 0.6;
+        this.context.font = `${font_height}px Courier New`;
+        let text_width = this.context.measureText(text).width;
+        if( text_width > w * 0.8) {
+            font_height *= w * 0.8 / text_width;
+            this.context.font = `${font_height}px Courier New`;
+            text_width = this.context.measureText(text).width;
+        }
+        this.context.fillStyle = 'rgba(0, 0, 0, 1)';
+        this.context.fillText(text, w/2 + x - text_width/2, h/2 + y + font_height/4);
+    }
+
     drawBoard() {
-        roundedRect(this.context, 0, 0, this.width, this.height, 10);
         this.context.fillStyle = this.texture;
-        this.context.fill();
+        this.roundedRectFilled(0, 0, this.width, this.height, this.margin.x);
     }
 
     setHangs() {
         this.hangboard.update(this.editor.value);
-        this.padding = 0.2 * 1/this.hangboard.size;
+        this.padding_ratio = 0.2 * 1/this.hangboard.size;
         this.draw();
+    }
+
+    roundedRectFilled(x,y,width,height,radius){
+        this.context.beginPath();
+        this.context.moveTo(x,y+radius);
+        this.context.lineTo(x,y+height-radius);
+        this.context.arcTo(x,y+height,x+radius,y+height,radius);
+        this.context.lineTo(x+width-radius,y+height);
+        this.context.arcTo(x+width,y+height,x+width,y+height-radius,radius);
+        this.context.lineTo(x+width,y+radius);
+        this.context.arcTo(x+width,y,x+width-radius,y,radius);
+        this.context.lineTo(x+radius,y);
+        this.context.arcTo(x,y,x,y+radius,radius);
+        this.context.fill();
+        this.context.stroke();
     }
 };
 
-function roundedRect(ctx,x,y,width,height,radius){
-    ctx.beginPath();
-    ctx.moveTo(x,y+radius);
-    ctx.lineTo(x,y+height-radius);
-    ctx.arcTo(x,y+height,x+radius,y+height,radius);
-    ctx.lineTo(x+width-radius,y+height);
-    ctx.arcTo(x+width,y+height,x+width,y+height-radius,radius);
-    ctx.lineTo(x+width,y+radius);
-    ctx.arcTo(x+width,y,x+width-radius,y,radius);
-    ctx.lineTo(x+radius,y);
-    ctx.arcTo(x,y,x,y+radius,radius);
-    ctx.stroke();
-}
-
 let canvas;
+let timer;
 
 window.onload = function() {
     canvas = new Canvas();
+    timer = new Timer('timer');
+    timer.start(20);    
     console.log(canvas);
 }
 
